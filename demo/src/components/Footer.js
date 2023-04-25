fe8ab9e631b4281f0d0eb2fad327ae8826cf58bc
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-04-25 11:18:04 +0000

    lab

---
+++ demo/src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    let Facelink="https://www.facebook.com/";
+    return(
+        <div>
+
+            <h1> The Author of this document is Amro </h1> 
+            <a href={Facelink}> <h1> our Facebook page</h1></a>
+        </div>
+    )
+
+}
+export default Footer;
